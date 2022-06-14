1d945b686374d1c49c324b4ee26756e219f193a8
Ankit Rawani <ankitgdes@gmail.com>
2022-06-14 08:21:05 +0530

    Add: functions to query admin model

---
+++ prisma/user-master/admin.js
@@ -0,0 +1,50 @@
+import { prisma } from '../prisma';
+
+// Create Admin
+export const createAdmin = async (data) => {
+  const admin = await prisma.admin.create({
+    data: data
+  });
+
+  return admin;
+};
+
+// Read Admin
+export const getAdmin = async (id) => {
+  const admin = await prisma.admin.findUnique({
+    where: { id }
+  });
+
+  return admin;
+};
+
+// Check Admin
+export const checkAdmin = async (username, email, phone) => {
+  console.log(username);
+
+  let admin = await prisma.admin.findUnique({
+    where: { username }
+  });
+
+  if (!admin)
+    admin = await prisma.admin.findUnique({
+      where: { email }
+    });
+
+  if (!admin)
+    admin = await prisma.admin.findUnique({
+      where: { phone }
+    });
+
+  return admin;
+};
+
+// Update Admin
+export const updateAdmin = async (id, updateData) => {
+  const admin = await prisma.admin.update({
+    where: { id },
+    data: { ...updateData }
+  });
+
+  return admin;
+};
