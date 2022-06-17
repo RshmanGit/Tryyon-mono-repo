80f4cf319847e98de1cd7e0fb5f18e69e54e4b8f
Ankit Rawani <ankitgdes@gmail.com>
2022-06-17 14:09:03 +0530

    Add: CRUD api endpoints for products model

---
+++ pages/api/products/index.js
@@ -0,0 +1,68 @@
+import async from 'async';
+
+import {
+  getAllProducts,
+  getAllProductsPaginated
+} from '../../../prisma/products/products';
+import handleResponse from '../../../utils/helpers/handleResponse';
+
+const handler = async (req, res) => {
+  if (req.method == 'GET') {
+    async.auto(
+      {
+        read: [
+          async () => {
+            const { paginated, count, offset } = req.query;
+
+            if (paginated == 'true' && (!count || !offset)) {
+              throw new Error(
+                JSON.stringify({
+                  errorKey: 'read',
+                  body: {
+                    status: 422,
+                    data: {
+                      message:
+                        'Missing query parameter, both count and offset need with paginated'
+                    }
+                  }
+                })
+              );
+            }
+
+            let products;
+            if (paginated)
+              products = await getAllProductsPaginated(
+                Number(offset),
+                Number(count)
+              );
+            else products = await getAllProducts();
+
+            if (products) {
+              return {
+                message: 'Products found',
+                products
+              };
+            }
+
+            throw new Error(
+              JSON.stringify({
+                errorKey: 'read',
+                body: {
+                  status: 404,
+                  data: {
+                    message: 'No Product found'
+                  }
+                }
+              })
+            );
+          }
+        ]
+      },
+      handleResponse(req, res, 'read')
+    );
+  } else {
+    res.send(405).json({ message: 'Method Not Allowed' });
+  }
+};
+
+export default handler;
