3c6aa734d468efdebd9a69aa5c63c797285b4bd0
Ankit Rawani <ankitgdes@gmail.com>
2022-06-24 19:01:01 +0530

    Add: new middleware for admin and userauth

---
+++ utils/middlewares/auth.js
@@ -0,0 +1,39 @@
+import jwt from 'jsonwebtoken';
+
+// Checking if the user is an admin or an authenticated user and setting req.admin and/or req.user
+const auth = (req, res, next) => {
+  if (!req.headers['authorization']) {
+    return res
+      .status(403)
+      .json({ message: 'A token is required for authentication' });
+  }
+
+  const token = req.headers['authorization'].replace('Bearer ', '');
+
+  let isAuthenticatedUser = false,
+    isAdmin = false;
+
+  try {
+    const decoded = jwt.verify(token, process.env.ADMIN_TOKEN_KEY);
+    req.admin = decoded;
+    isAdmin = true;
+  } catch (err) {
+    console.log('Not an admin user');
+  }
+
+  try {
+    const decoded = jwt.verify(token, process.env.TOKEN_KEY);
+    req.user = decoded;
+    isAuthenticatedUser = true;
+  } catch (err) {
+    console.log('Not an authenticated user');
+  }
+
+  if (!isAdmin & !isAuthenticatedUser) {
+    return res.status(401).json({ message: 'Unauthorised Admin' });
+  }
+
+  return next();
+};
+
+export default auth;
