f44163226037bb4bbec86b3510cdc8990272cf6d
Ankit Rawani <ankitgdes@gmail.com>
2022-07-05 13:13:23 +0530

    Add: table and searchbar components

---
+++ ui/components/table/index.js
@@ -0,0 +1,124 @@
+import {
+  Flex,
+  Table,
+  Tbody,
+  Td,
+  Text,
+  Th,
+  Thead,
+  Tr,
+  useColorModeValue
+} from '@chakra-ui/react';
+import React, { useMemo } from 'react';
+import {
+  useGlobalFilter,
+  usePagination,
+  useSortBy,
+  useTable
+} from 'react-table';
+
+// Custom components
+import Card from '../card/Card';
+
+export default function TableComp(props) {
+  const { columnsData, tableData } = props;
+
+  const columns = useMemo(() => columnsData, [columnsData]);
+  const data = useMemo(() => tableData, [tableData]);
+
+  const tableInstance = useTable(
+    {
+      columns,
+      data
+    },
+    useGlobalFilter,
+    useSortBy,
+    usePagination
+  );
+
+  const {
+    getTableProps,
+    getTableBodyProps,
+    headerGroups,
+    page,
+    prepareRow,
+    initialState
+  } = tableInstance;
+  // initialState.pageSize = 5;
+
+  const textColor = useColorModeValue('secondaryGray.900', 'white');
+  const borderColor = useColorModeValue('gray.200', 'whiteAlpha.100');
+  return (
+    <Card
+      direction="column"
+      w="100%"
+      px="0px"
+      overflowX={{ sm: 'scroll', lg: 'scroll' }}
+    >
+      <Table {...getTableProps()} variant="simple" color="gray.500" mb="24px">
+        <Thead>
+          {headerGroups.map((headerGroup, index) => (
+            <Tr {...headerGroup.getHeaderGroupProps()} key={index}>
+              {headerGroup.headers.map((column, index) => (
+                <Th
+                  {...column.getHeaderProps(column.getSortByToggleProps())}
+                  pe="10px"
+                  key={index}
+                  borderColor={borderColor}
+                >
+                  <Flex
+                    justify="space-between"
+                    align="center"
+                    fontSize={{ sm: '10px', lg: '12px' }}
+                    color="gray.400"
+                  >
+                    {column.render('Header')}
+                  </Flex>
+                </Th>
+              ))}
+            </Tr>
+          ))}
+        </Thead>
+        <Tbody {...getTableBodyProps()}>
+          {page.map((row, index) => {
+            prepareRow(row);
+            return (
+              <Tr {...row.getRowProps()} key={index}>
+                {row.cells.map((cell, index) => {
+                  let data = '';
+                  columns.forEach((col) => {
+                    if (cell.column.Header === col.Header) {
+                      data = (
+                        <Flex align="center">
+                          <Text
+                            color={textColor}
+                            fontSize="sm"
+                            fontWeight="400"
+                          >
+                            {cell.value}
+                          </Text>
+                        </Flex>
+                      );
+                    }
+                  });
+
+                  return (
+                    <Td
+                      {...cell.getCellProps()}
+                      key={index}
+                      fontSize={{ sm: '14px' }}
+                      minW={{ sm: '150px', md: '200px', lg: 'auto' }}
+                      borderColor="transparent"
+                    >
+                      {data}
+                    </Td>
+                  );
+                })}
+              </Tr>
+            );
+          })}
+        </Tbody>
+      </Table>
+    </Card>
+  );
+}
