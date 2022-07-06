73ad3bdb7f48ca26f78471f1bb21ba31598a6da4
Ankit Rawani <ankitgdes@gmail.com>
2022-07-06 11:16:11 +0530

    Add: redirect from /admin to /admin/default

--- next.config.js
+++ next.config.js
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true
+  reactStrictMode: true,
+  async redirects() {
+    return [
+      {
+        source: '/admin',
+        destination: '/admin/default',
+        permanent: true
+      }
+    ];
+  }
 };
 
 module.exports = nextConfig;
