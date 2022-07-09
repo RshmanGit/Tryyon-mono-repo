9305630851737f13dee073f2c8396a1051df422e
Ankit Rawani <ankitgdes@gmail.com>
2022-07-09 18:41:28 +0530

    Add: facets

--- pages/admin/products.js
+++ pages/admin/products.js
@@ -38,6 +38,7 @@ import { ChevronDownIcon } from '@chakra-ui/icons';
 
 import { SearchBar } from '../../ui/components/searchbar';
 import useDebounce from '../../utils/hooks/useDebounce';
+import Facet from '../../ui/components/facet';
 
 const columnsData = [
   {
@@ -83,6 +84,8 @@ export default function ProductPage() {
   const [data, setData] = useState([]);
   const [page, setPage] = useState(0);
   const [searchString, setSearchString] = useState('');
+  const [categoryQuery, setCategoryQuery] = useState('');
+  const [attributesQuery, setAttributesQuery] = useState({});
 
   const [modalHeading, setModalHeading] = useState('');
   const [modalBody, setModalBody] = useState('');
@@ -103,8 +106,15 @@ export default function ProductPage() {
   const [attributeList, setAttributeList] = useState({ 0: ['', ''] });
   const [attributes, setAttributes] = useState({});
   const [availableAttributes, setAvailableAttributes] = useState([]);
+  const [priceFrom, setPriceFrom] = useState(0);
+  const [priceTo, setPriceTo] = useState(0);
 
-  const debouncedSearchString = useDebounce(searchString, 800);
+  const debouncedSearchString = useDebounce(searchString, 1500);
+  const debouncedCategoryQuery = useDebounce(categoryQuery, 1500);
+  const debouncedAttributesQuery = useDebounce(attributesQuery, 1500);
+  const debouncedPriceFrom = useDebounce(priceFrom, 1500);
+  const debouncedPriceTo = useDebounce(priceTo, 1500);
+
   const { isOpen, onOpen, onClose } = useDisclosure();
 
   const router = useRouter();
@@ -458,28 +468,61 @@ export default function ProductPage() {
   useEffect(() => {
     let query = {},
       rest = '';
+
     const accepted_values = [
       'id',
       'inStock',
       'published',
       'supplierId',
-      'categoryId'
+      'priceFrom',
+      'priceTo'
     ];
+
     const q = debouncedSearchString.split(/\s+/);
+
     q.forEach((e) => {
       let a = e.split(':');
       if (a.length == 2 && accepted_values.indexOf(a[0]) != -1) {
         if (a[0] == 'inStock' || a[0] == 'published') {
           query[a[0]] = a[1] == 'true';
+        } else if (a[0] == 'priceFrom' || a[0] == 'priceTo') {
+          try {
+            query[a[0]] = parseInt(a[1], 10);
+          } catch (err) {
+            toast({
+              title: err.message,
+              status: 'error',
+              duration: 2000,
+              isClosable: true
+            });
+          }
         } else query[a[0]] = a[1];
       } else {
         if (rest != '') rest += ' ';
         rest += e;
       }
     });
+
     if (rest) {
       query.query = rest;
     }
+
+    if (debouncedCategoryQuery != '') {
+      query.categoryId = debouncedCategoryQuery.id;
+    }
+
+    if (debouncedAttributesQuery != {}) {
+      query.attributes = debouncedAttributesQuery;
+    }
+
+    if (debouncedPriceFrom > 0) {
+      query.priceFrom = debouncedPriceFrom;
+    }
+
+    if (debouncedPriceTo > 0) {
+      query.priceTo = debouncedPriceTo;
+    }
+
     console.log(query);
     fetch(`${router.basePath}/api/products`, {
       method: 'POST',
@@ -526,7 +569,15 @@ export default function ProductPage() {
           isClosable: true
         });
       });
-  }, [router, debouncedSearchString, toast]);
+  }, [
+    router,
+    debouncedSearchString,
+    toast,
+    debouncedAttributesQuery,
+    debouncedCategoryQuery,
+    debouncedPriceFrom,
+    debouncedPriceTo
+  ]);
 
   useEffect(() => {
     if (localStorage.page)
@@ -566,6 +617,18 @@ export default function ProductPage() {
             </Button>
           </SearchBar>
         </Flex>
+
+        <Facet
+          category={categoryQuery}
+          attributes={attributesQuery}
+          setCategory={setCategoryQuery}
+          setAttributes={setAttributesQuery}
+          priceFrom={priceFrom}
+          priceTo={priceTo}
+          setPriceFrom={setPriceFrom}
+          setPriceTo={setPriceTo}
+        />
+
         <TableComp
           editEntry={openEdit}
           deleteEntry={openDelete}
