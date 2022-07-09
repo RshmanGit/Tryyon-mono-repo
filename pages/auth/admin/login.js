868f7f60724bc570494ad3bc383feb14ca83e631
Mayank <mayank30.mj@gmail.com>
2022-07-09 11:42:16 +0530

    Modified 3 variants adding feature to 5

--- pages/auth/admin/login.js
+++ pages/auth/admin/login.js
@@ -98,6 +98,7 @@ function Login() {
               .then((res) => {
                 if (res.message === 'admin Authenticated') {
                   sessionStorage.setItem('token', 1);
+                  // alert(res.message);
                   setButtonText('Admin Authenticated');
                   return res;
                 } else {
